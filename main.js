3e07190c025eae9ca9ea9da6133dd0600d234002
lowerkey <ivan14@hotmail.co.uk>
2023-11-04 11:48:09 +0000

    metamask

--- main.js
+++ main.js
@@ -25,6 +25,32 @@ function setupThemeToggle() {
       }
     });
   }
+
+const showAccount = document.querySelector('.showAccount');
+function setupMetaMask(){
+  const ethereumButton = document.querySelector('.enableEthereumButton');
+  
+  
+  ethereumButton.addEventListener('click', () => {
+    getAccount();
+  });
+}
+
+  async function getAccount() {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      .catch((err) => {
+        if (err.code === 4001) {
+          // EIP-1193 userRejectedRequest error
+          // If this happens, the user rejected the connection request.
+          console.log('Please connect to MetaMask.');
+        } else {
+          console.error(err);
+        }
+      });
+    const account = accounts[0];
+    showAccount.innerHTML = account;
+  }
+
   
   fetch('navbar.html')
     .then(response => response.text())
@@ -32,5 +58,7 @@ function setupThemeToggle() {
       document.getElementById('navbar-placeholder').innerHTML = data;
       // Call the setupThemeToggle function after the navbar is created
       setupThemeToggle();
+      setupMetaMask();
     });
   
+
